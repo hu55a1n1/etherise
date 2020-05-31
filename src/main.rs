910103a5c1f4ef53a7cc1fc1bddc778b67567cf8
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-05-31 02:55:40 +0200

    Add opt for rlp --decode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::error;
 
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
+
+use etherise::eth1::*;
+use rlp::decode;
+use rustc_hex::FromHex;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let matches = App::new("etherise")
@@ -11,6 +15,13 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             .short("v")
             .multiple(true)
             .help("Sets the level of verbosity"))
+        .subcommand(SubCommand::with_name("rlp")
+            .about("Recursive-length prefix (RLP)")
+            .arg(Arg::with_name("decode")
+                .short("d")
+                .long("decode")
+                .takes_value(true)
+                .help("decode RLP data")))
         .get_matches();
 
     match matches.occurrences_of("v") {
@@ -18,6 +29,14 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         1 => println!("Some verbose info"),
         2 => println!("Tons of verbose info"),
         3 | _ => println!("Don't be crazy"),
+    }
+
+    if let Some(matches) = matches.subcommand_matches("rlp") {
+        if let Some(hex) = matches.value_of("decode") {
+            let bytes = FromHex::from_hex(&hex[2..]).unwrap();
+            let rlp = decode::<UnverifiedTransaction>(bytes.as_slice())?;
+            println!("{:#?}", rlp);
+        }
     }
 
     Ok(())
