3829233480539b2d8e26665c5a7d48cb79f7edc9
Shoaib Ahmed <shoaib.ahmed@slock.it>
2020-06-13 21:54:21 +0200

    Impl HexSlice and extension trait for hex display

--- src/eth1/mod.rs
+++ src/eth1/mod.rs
@@ -1,4 +1,41 @@
-pub use self::types::*;
+use std::fmt;
 
-pub mod types;
+pub use self::rlp::*;
+
+pub mod rlp;
+
+struct HexSlice<'a>(&'a [u8]);
+
+impl<'a> HexSlice<'a> {
+    fn new<T>(data: &'a T) -> HexSlice<'a>
+        where T: ?Sized + AsRef<[u8]> + 'a {
+        HexSlice(data.as_ref())
+    }
+
+    fn to_string(&self) -> String {
+        format!("{}", self)
+    }
+}
+
+impl fmt::Display for HexSlice<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "0x")?;
+        for byte in self.0 {
+            write!(f, "{:02x}", byte)?;
+        }
+        Ok(())
+    }
+}
+
+trait HexDisplayExt {
+    fn hex_display(&self) -> HexSlice<'_>;
+}
+
+impl<T> HexDisplayExt for T
+    where T: ?Sized + AsRef<[u8]> {
+    fn hex_display(&self) -> HexSlice<'_> {
+        HexSlice::new(self)
+    }
+}
+
 
